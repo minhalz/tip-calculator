52797733cb56ca814fc1e596a9658053cfa53438
Minhal Zaidi <minhal.zaidi@student.uts.edu.au>
2021-08-15 19:00:46 +1000

    Add padding on mobile view

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
       <Head>
         <title>Tip Calculator</title>
       </Head>
-      <div className="flex flex-col justify-center items-center min-h-screen">
-        <div className="mb-10">
+      <div className="flex flex-col justify-center items-center py-7 md:py-10 min-h-screen">
+        <div className="mb-7 md:mb-10">
           <Image src={Logo} alt="Splitter logo" />
         </div>
         <Calculator />
